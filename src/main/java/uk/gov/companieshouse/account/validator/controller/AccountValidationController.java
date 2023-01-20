7cc1f6855e9923240b8b7cd7ecd85de66e3a07ae
Harry Bosworth <hbosworth@companieshouse.gov.uk>
2023-01-20 11:27:52 +0000

    Removed unused method

--- src/main/java/uk/gov/companieshouse/account/validator/controller/AccountValidationController.java
+++ src/main/java/uk/gov/companieshouse/account/validator/controller/AccountValidationController.java
@@ -46,35 +46,4 @@ public class AccountValidationController {
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
         }
     }
-
-
-    @PostMapping("/mock_validate")
-    public ResponseEntity<ResponseMessage> mockValidate(@Valid @RequestBody FileDetails fileDetails) {
-        String message = "";
-        try {
-
-            String uuid = UUID.randomUUID().toString();
-            AccountValidated accountValidated = new AccountValidated();
-            accountValidated.setId(uuid);
-            accountValidated.setCustomerId("1234567890");
-            accountValidated.setFilename("account.zip");
-            accountValidated.setS3Key("697e40ba-cc6a-4b40-967c-6b4cdde8af23");
-            accountValidated.setStatus("Complete");
-            ValidationResponse validationResponse = new ValidationResponse();
-            validationResponse.setId(uuid);
-            validationResponse.setValid(true);
-            validationResponse.setTndpResponse("OK");
-
-            accountValidated.setData(validationResponse);
-            accountValidated.setShowImage(false);
-
-            accountValidatedService.createAccount(accountValidated);
-
-            message = "Validation saved successfully";
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-        } catch (Exception e) {
-            message = "Could not save the validation: " + fileDetails.getFile_name() + ". Error: " + e.getMessage();
-            return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
-        }
-    }
 }
